0758242a63beb3b2b133e06d719655965deb7e7d
Thiago Verissimo da Silva Campos <thiagovsc96@gmail.com>
2023-04-15 18:59:04 +0000

    first commit

--- src/index.js
+++ src/index.js
@@ -23,21 +23,23 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository)
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  const repositoryIndex = repositories.findindex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
   const repository = { ...repositories[repositoryIndex], ...updatedRepository };
-
+console.log(repository)
   repositories[repositoryIndex] = repository;
 
   return response.json(repository);
